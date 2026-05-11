8fcf79ec2255a23e7780526b282f541da1f9d50b
Yuan <49439486+Sky-And-Ground@users.noreply.github.com>
2026-05-11 20:19:46 +0800

    Update popcap_pak_extractor.cpp

--- popcap_pak_extractor.cpp
+++ popcap_pak_extractor.cpp
@@ -1,6 +1,6 @@
 /**
  * @author yuan
- * @brief PopCap's .pak file extractor, written in C++20, only works for windows platform.
+ * @brief PopCap's .pak file extractor, written in C++17, only works for windows platform.
  * 
  * a very big thanks to https://github.com/nathaniel-daniel/popcap-pak-rs for giving 
  * the popcap .pak file's format:
@@ -22,6 +22,10 @@
  *   end
  * 
 */
+#ifndef _WIN32
+#error "this program only works for windows platform"
+#endif
+
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
@@ -62,16 +66,24 @@ namespace parser {
         InvalidVersionException() : ParserError{ "invalid version field" } {}
     };
 
-    class FileBrokenException : public ParserError {
+    class UnexpectedEndOfFileException : public ParserError {
     public:
-        FileBrokenException() : ParserError{ "file maybe broken" } {}
+        UnexpectedEndOfFileException() : ParserError{ "file maybe broken" } {}
     };
 
     class WinFile {
         HANDLE handle;
-        DWORD fileSize;
     public:
-        WinFile() : handle{ INVALID_HANDLE_VALUE }, fileSize{ 0 } {}
+        WinFile() : handle{ INVALID_HANDLE_VALUE } {}
+
+        WinFile(const std::string& filePath, DWORD desiredAccess, DWORD creationDisposition) {
+            handle = CreateFileA(filePath.data(), desiredAccess, 0, nullptr, creationDisposition, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+            if (handle == INVALID_HANDLE_VALUE) {
+                std::error_code ec{ (int)GetLastError(), std::system_category() };
+                throw std::system_error{ ec, "CreateFileA failed on " + filePath };
+            }
+        }
 
         ~WinFile() {
             if (handle != INVALID_HANDLE_VALUE) {
@@ -79,49 +91,60 @@ namespace parser {
             }
         }
 
-        void open_file_to_read(const std::string& filePath) {
-            handle = CreateFileA(filePath.c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-            if (handle == INVALID_HANDLE_VALUE) {
-                throw FileNotFoundException{ filePath };
-            }
+        WinFile(const WinFile&) = delete;
+        WinFile& operator=(const WinFile&) = delete;
 
-            fileSize = GetFileSize(handle, nullptr);
-            if (fileSize == INVALID_FILE_SIZE) {
-                throw std::system_error(GetLastError(), std::system_category(), "GetFileSize failed");
-            }
+        WinFile(WinFile&& other) : handle{ other.handle } {
+            other.handle = INVALID_HANDLE_VALUE;
         }
 
-        void open_file_to_write(const std::string& filePath) {
-            handle = CreateFileA(filePath.c_str(), GENERIC_WRITE, 0, nullptr, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, nullptr);
-            if (handle == INVALID_HANDLE_VALUE) {
-                throw std::system_error(GetLastError(), std::system_category(), "CreateFileA failed");
+        WinFile& operator=(WinFile&& other) {
+            if (this != &other) {
+                if (handle != INVALID_HANDLE_VALUE) {
+                    CloseHandle(handle);
+                }
+
+                handle = other.handle;
+                other.handle = INVALID_HANDLE_VALUE;
             }
+
+            return *this;
         }
 
         DWORD read(char* buf, DWORD numOfBytesToRead) {
-            if (fileSize < numOfBytesToRead) {
-                throw FileBrokenException{};
-            }
-
             DWORD numOfBytesRead;
+
             if (!ReadFile(handle, (LPVOID)buf, numOfBytesToRead, &numOfBytesRead, nullptr)) {
-                throw std::system_error(GetLastError(), std::system_category(), "ReadFile failed");
+                std::error_code ec{ (int)GetLastError(), std::system_category() };
+                throw std::system_error{ ec, "ReadFile failed" };
             }
 
-            fileSize -= numOfBytesRead;
             return numOfBytesRead;
         }
 
         void write(const char* buf, DWORD numOfBytesToWrite) {
             if (!WriteFile(handle, buf, numOfBytesToWrite, nullptr, nullptr)) {
-                throw std::system_error(GetLastError(), std::system_category(), "WriteFile failed");
+                std::error_code ec{ (int)GetLastError(), std::system_category() };
+                throw std::system_error{ ec, "WriteFile failed" };
             }
         }
 
         void set_file_time(const FILETIME& ft) {
             if (!SetFileTime(handle, nullptr, nullptr, &ft)) {
-                throw std::system_error(GetLastError(), std::system_category(), "SetFileTime failed");
+                std::error_code ec{ (int)GetLastError(), std::system_category() };
+                throw std::system_error{ ec, "SetFileTime failed" };
             }
+        }
+
+        DWORD get_file_size() {
+            DWORD fileSize = GetFileSize(handle, nullptr);
+
+            if (fileSize == INVALID_FILE_SIZE) {
+                std::error_code ec{ (int)GetLastError(), std::system_category() };
+                throw std::system_error{ ec, "GetFileSize failed" };
+            }
+
+            return fileSize;
         }
     };
 
@@ -132,123 +155,124 @@ namespace parser {
     };
 
     class Parser {
-        std::array<UCHAR, 4> magic;
-        std::array<UCHAR, 4> version;
         std::vector<FileAttr> fileAttrs;
-        WinFile wf;
+        WinFile pak;
+        DWORD pakSize;
 
-        template<typename CharType>
-        UCHAR decode_one_byte(CharType c) noexcept {
-            // using 0xf7 to decode the data in .pak file.
-            return static_cast<UCHAR>(c ^ 0xf7);
+        std::unique_ptr<char[]> create_buffer(size_t size) {
+            return std::make_unique<char[]>(size);
         }
 
-        template<typename CharType>
-        void decode_bytes(CharType* data, size_t len) noexcept {
-            for (size_t i = 0; i < len; ++i) {
-                data[i] = decode_one_byte(data[i]);
+        void read_from_pak(char* buf, DWORD bytesToRead) {
+            if (pakSize < bytesToRead) {
+                throw UnexpectedEndOfFileException{};
             }
+
+            DWORD total = bytesToRead;
+
+            while (total > 0) {
+                DWORD read = pak.read(buf, total);
+
+                buf += read;
+                total -= read;
+            }
+
+            // decode them.
+            for (DWORD i = 0; i < bytesToRead; ++i) {
+                buf[i] = ((unsigned char)buf[i]) ^ 0xf7;
+            }
+
+            pakSize -= bytesToRead;
         }
 
         bool reach_header_end() {
-            char c;
-            wf.read(&c, sizeof(c));
-            return decode_one_byte(c) == 0x80;
-        }
+            UCHAR c;
 
-        bool check_magic() noexcept {
-            return magic[0] == 0xC0 
-                && magic[1] == 0x4A 
-                && magic[2] == 0xC0 
-                && magic[3] == 0xBA;
-        }
-
-        bool check_version() noexcept {
-            return version[0] == 0x00 
-                && version[1] == 0x00 
-                && version[2] == 0x00 
-                && version[3] == 0x00;
+            read_from_pak((char*)(&c), sizeof(c));
+            return c == 0x80;
         }
 
         void parse_magic() {
-            wf.read((char*)magic.data(), magic.size());
-            decode_bytes(magic.data(), magic.size());
+            std::array<UCHAR, 4> magic;
 
-            if (!check_magic()) {
+            read_from_pak((char*)magic.data(), magic.size());
+
+            bool check_magic = magic[0] == 0xC0 
+                            && magic[1] == 0x4A 
+                            && magic[2] == 0xC0 
+                            && magic[3] == 0xBA;
+
+            if (!check_magic) {
                 throw InvalidMagicException{};
             }
         }
 
         void parse_version() {
-            wf.read((char*)version.data(), version.size());
-            decode_bytes(version.data(), version.size());
+            std::array<UCHAR, 4> version;
 
-            if (!check_version()) {
+            read_from_pak((char*)version.data(), version.size());
+
+            bool check_version = version[0] == 0x00 
+                            && version[1] == 0x00 
+                            && version[2] == 0x00 
+                            && version[3] == 0x00;
+
+            if (!check_version) {
                 throw InvalidVersionException{};
             }
         }
 
-        std::unique_ptr<char[]> init_file_name(size_t size) {
-            return std::unique_ptr<char[]>(new char[size]);
-        }
-
         void parse_file_name(FileAttr& attr) {
             char c;
-            wf.read(&c, sizeof(char));
+
+            read_from_pak(&c, sizeof(char));
 
             // get the length of the file name.
-            uint32_t fileNameLen = (uint32_t)decode_one_byte(c);
-            attr.fileName = init_file_name(fileNameLen + 1);
-            attr.fileName[fileNameLen] = '\0';
+            uint32_t fileNameLen = (uint32_t)c;
+            attr.fileName = create_buffer(fileNameLen + 1);
 
             // get file name.
-            wf.read(attr.fileName.get(), fileNameLen);
-            decode_bytes(attr.fileName.get(), fileNameLen);
+            read_from_pak(attr.fileName.get(), fileNameLen);
+            attr.fileName[fileNameLen] = '\0';
         }
 
         void parse_file_size(FileAttr& attr) {
-            constexpr uint32_t FILE_SIZE_BYTES = 4;
-            wf.read((char*)(&(attr.fileSize)), FILE_SIZE_BYTES);
-            decode_bytes((char*)(&(attr.fileSize)), FILE_SIZE_BYTES);
+            read_from_pak((char*)(&(attr.fileSize)), sizeof(attr.fileSize));
         }
 
         void parse_file_last_write_time(FileAttr& attr) {
-            wf.read((char*)(&(attr.lastWriteTime)), sizeof(FILETIME));
-            decode_bytes((char*)(&(attr.lastWriteTime)), sizeof(FILETIME));
+            read_from_pak((char*)(&(attr.lastWriteTime)), sizeof(FILETIME));
         }
 
-        template<size_t N>
-        void save_single_file(const FileAttr& attr, std::array<char, N>& buf, const fs::path& p) {
+        void save_single_file(const FileAttr& attr, char* buf, size_t buffer_size, const fs::path& p) {
             DWORD fileSize = attr.fileSize;
-            DWORD readLen = 0;
             DWORD needLen = 0;
             
-            WinFile tmpFile;
-            tmpFile.open_file_to_write(p.string());
+            WinFile tmpFile { p.string(), GENERIC_WRITE, CREATE_NEW };
 
             while (fileSize > 0) {
-                needLen = (fileSize < buf.size() ? fileSize : buf.size());
-                readLen = wf.read(buf.data(), needLen);
+                needLen = (fileSize < buffer_size ? fileSize : buffer_size);
 
-                decode_bytes(buf.data(), buf.size());
-                tmpFile.write(buf.data(), readLen);
+                read_from_pak(buf, needLen);
+                tmpFile.write(buf, needLen);
 
-                fileSize -= readLen;
+                fileSize -= needLen;
             }
 
             tmpFile.set_file_time(attr.lastWriteTime);
         }
     public:
-        Parser() {}
+        Parser(const std::string& filePath) 
+            : fileAttrs{}, pak{ filePath, GENERIC_READ, OPEN_EXISTING }, pakSize{ INVALID_FILE_SIZE } 
+        {
+            pakSize = pak.get_file_size();
+        }
 
         const std::vector<FileAttr>& get_file_attrs() const noexcept {
             return fileAttrs;
         }
 
-        void open(const std::string& filePath) {
-            wf.open_file_to_read(filePath);
-        }
-
+        // this function would throw ParseError or std::system_error.
         void parse_header() {
             parse_magic();
             parse_version();
@@ -268,8 +292,10 @@ namespace parser {
             }
         }
 
+        // this function would throw ParseError or std::system_error.
         void save_body(const std::string& toDir) {
-            std::array<char, 8192> buf;
+            const size_t buffer_size = 65536;
+            std::unique_ptr<char[]> buf = create_buffer(buffer_size);
 
             for (const FileAttr& attr : fileAttrs) {
                 fs::path p{ toDir };
@@ -280,15 +306,13 @@ namespace parser {
                     fs::create_directories(parentDir);
                 }
 
-                save_single_file(attr, buf, p);
+                save_single_file(attr, buf.get(), buffer_size, p);
             }
         }
     };
 };
 
-// not thread safe.
-const char* format_windows_filetime(const FILETIME& ft) {
-    static char buf[32];
+const char* format_windows_filetime(const FILETIME& ft, char* buf, size_t buf_size) {
     ULARGE_INTEGER ull;
     
     ull.LowPart = ft.dwLowDateTime;
@@ -303,15 +327,16 @@ const char* format_windows_filetime(const FILETIME& ft) {
     */
     time_t timestamp = (time_t)((ull.QuadPart / 10000000ULL) - 11644473600ULL);
     struct tm* timeinfo = localtime(&timestamp);
-    strftime(buf, sizeof(buf) / sizeof(char), "%Y-%m-%d %H:%M:%S", timeinfo);
+    strftime(buf, buf_size, "%Y-%m-%d %H:%M:%S", timeinfo);
     return buf;
 }
 
 void save_file_attrs(const std::vector<parser::FileAttr>& attrs, const std::string& savFile) {
     std::ofstream out{ savFile };
+    char buf[32];
 
     for (const parser::FileAttr& attr : attrs) {
-        out << format_windows_filetime(attr.lastWriteTime) << "  " << std::setw(10) << attr.fileSize << " bytes  " << attr.fileName.get() << "\n";
+        out << format_windows_filetime(attr.lastWriteTime, buf, std::size(buf)) << "  " << std::setw(10) << attr.fileSize << " bytes  " << attr.fileName.get() << "\n";
     }
 
     out.close();
@@ -324,29 +349,17 @@ int main(int argc, char* argv[]) {
     }
 
     if (fs::is_directory(argv[2])) {
-        std::cerr << "dir: " << argv[2] << " is not empty.\n";
+        std::cerr << "dir: " << argv[2] << " is already existed.\n";
         return 1;
     }
 
-    try {
-        parser::Parser pakParser;
-        
-        pakParser.open(argv[1]);
-        pakParser.parse_header();
-        pakParser.save_body(argv[2]);
+    parser::Parser pakParser{ argv[1] };
+    
+    pakParser.parse_header();
+    pakParser.save_body(argv[2]);
+    std::cout << "file body saved -> " << argv[2] << "\n";
 
-        save_file_attrs(pakParser.get_file_attrs(), "file_attrs.txt");
-        std::cout << "parse success, file attributes has been wriiten to `file_attrs.txt`\n";
-    }
-    catch(const parser::ParserError& pe) {
-        std::cerr << "parser error, " << pe.what() << "\n";
-    }
-    catch(const std::system_error& se) {
-        std::cerr << "system error, " << se.code() << ", " << se.what() << "\n";
-    }
-    catch(const std::exception& e) {
-        std::cerr << "standard exception, " << e.what() << "\n";
-    }
-
+    save_file_attrs(pakParser.get_file_attrs(), "file_attrs.txt");
+    std::cout << "file attributes saved -> file_attrs.txt\n";
     return 0;
 }
