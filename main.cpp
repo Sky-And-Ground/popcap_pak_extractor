056ae33311385821a3c920ae3eb87d2e450b51d6
sky_and_ground <yangtianyuan2023@163.com>
2026-09-25 11:29:35 +0800

    当解析过程失败时，尝试将pak解压出来的文件全部删掉

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 #include <string>
 #include <system_error>
+#include <filesystem>
 #include <stdexcept>
+
+namespace fs = std::filesystem;
 
 void parse(const std::string& pak_path, const std::string& pak_header_save_path, const std::string& pak_body_save_path) {
     Parser parser{ pak_path };
@@ -16,6 +19,15 @@ void parse(const std::string& pak_path, const std::string& pak_header_save_path,
     std::cout << "save body part success\n";
 }
 
+void when_extract_failed(const std::string& pak_body_save_path) {
+    try{
+        fs::remove_all(pak_body_save_path);
+    }
+    catch(const fs::filesystem_error& e) {
+        std::cerr << "remove dir failed: " << pak_body_save_path << "\n";
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "usage: " << argv[0] << " <.pak> <sav_dir>\n";
@@ -27,10 +39,12 @@ int main(int argc, char* argv[]) {
     }
     catch(const std::system_error& e) {
         std::cerr << "system error, " << e.code().value() << ", " << e.code().message() << "\n";
+        when_extract_failed(argv[2]);
         return -1;
     }
     catch(const std::exception& e) {
         std::cerr << "error, " << e.what() << "\n";
+        when_extract_failed(argv[2]);
         return -1;
     }
 
